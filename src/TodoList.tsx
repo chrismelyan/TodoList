01f881bd856ca9a98d61187d6762f65925c65149
chrismelyan <chris.shelpuk@gmail.com>
2022-02-07 14:34:45 +0300

    message css

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -6,13 +6,11 @@ type TodoListPropsType = {
     title: string
     tasks: Array<TaskType>
 }
-
 export type TaskType = {
     id: number
     title: string
     isDone: boolean
 }
-
 const TodoList = (props: TodoListPropsType) => {
     return (
             <div>
